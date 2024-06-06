dd97bee4b75ab69ba7cbdc86fd6f17198668461e
AndyHsieh <andy.hsieh@gaiatechs.com>
2024-06-06 14:59:42 +0800

    feat: add successful-case > meta page

---
+++ app/netronAdmin/(dashboard)/(successful-case)/meta/success/page.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import {
+  Form,
+} from "@/components/ui/form"
+import { useForm } from 'react-hook-form'
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import FormMetaSection, { metaSchema } from '@/components/netronAdmin/global/FormMetaSection'
+import { Button } from '@/components/netronAdmin/global/button'
+
+const formSchema = z.object(metaSchema)
+
+export default function MetaNewsPage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      metaTitle: "",
+      metaKeyword: "",
+      metaDescription: "",
+    },
+  })
+
+  async function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log(data);
+  }
+
+  return (
+    <>
+      <h2 className='text-3xl font-medium'>Meta 資訊</h2>
+      <section>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormMetaSection form={form} />
+            <div className="text-right">
+              <Button type="submit">儲存</Button>
+            </div>
+          </form>
+        </Form>
+      </section>
+    </>
+  )
+}
